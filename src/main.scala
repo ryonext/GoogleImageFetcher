4282fe913a45349511938f5e7dc7ca9805f7c255
ryonext <ryonext.s@gmail.com>
2016-02-03 16:35:45 +0900

    Implement accessing google search api.

---
+++ src/main.scala
@@ -0,0 +1,16 @@
+import scala.io._
+/**
+  * Created by ryonext on 2016/02/03.
+  */
+object main {
+  def main(args:Array[String]) = {
+    val apiKey = sys.env("GOOGLE_API_KEY")
+    val engineId = sys.env("SEARCH_ENGINE_ID")
+    val query = "寿司"
+    val url = "https://www.googleapis.com/customsearch/v1?key=" + apiKey + "&cx=" + engineId + "&q=" + query
+
+    val source = Source.fromURL(url)
+
+    print(source.getLines.mkString)
+  }
+}
